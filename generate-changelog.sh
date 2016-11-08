c685dcc94be65beb4893a2539d6805f6ed856388
ggranum <geoff.granum@gmail.com>
2016-11-08 14:08:43 +0000

    Update git tag script, comments.

--- generate-changelog.sh
+++ generate-changelog.sh
@@ -2,10 +2,6 @@
 # npm publish with goodies
 # prerequisites:
 # Create a github access token [https://github.com/settings/tokens] and save it in a file named 'generate-changelog-token.local.txt'
-# Run this script with required argument `patch`/`minor`/`major`/`<version>`
-# defaults to conventional-recommended-bump
-# and optional argument preset `angular`/ `jquery` ...
-# defaults to conventional-commits-detector
 
 
 githubToken=$(cat ./generate-changelog-token.local.txt)
@@ -16,7 +12,7 @@ git add CHANGELOG.md
 version=$(./node_modules/.bin/json -f package.json version)
 echo '============================ To commit changelog changes to git run the following ============================'
 echo 'git commit -m "docs(CHANGELOG): '$version'"'
-echo 'git tag '$version
+echo 'git tag v'$version
 echo 'git push --follow-tags'
 echo './node_modules/.bin/conventional-github-releaser -t ' ${githubToken} ' -p angular'
 echo '===================================================================================='
